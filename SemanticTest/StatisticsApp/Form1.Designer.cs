544f1c1e82c312702ff9c34c4ace070c44e23d44
savvas pamboukas <savvaspamboukas@hotmail.com>
2018-05-18 04:06:18 +0300

    completed

--- SemanticTest/StatisticsApp/Form1.Designer.cs
+++ SemanticTest/StatisticsApp/Form1.Designer.cs
@@ -28,12 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblMean = new System.Windows.Forms.Label();
+            this.SDlbl = new System.Windows.Forms.Label();
+            this.lblMeanResult = new System.Windows.Forms.Label();
+            this.lblStdDeviation = new System.Windows.Forms.Label();
+            this.lblHistogram = new System.Windows.Forms.Label();
+            this.lblHistogramResults = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMean
+            // 
+            this.lblMean.AutoSize = true;
+            this.lblMean.Location = new System.Drawing.Point(50, 47);
+            this.lblMean.Name = "lblMean";
+            this.lblMean.Size = new System.Drawing.Size(73, 29);
+            this.lblMean.TabIndex = 0;
+            this.lblMean.Text = "Mean";
+            // 
+            // SDlbl
+            // 
+            this.SDlbl.AutoSize = true;
+            this.SDlbl.Location = new System.Drawing.Point(50, 127);
+            this.SDlbl.Name = "SDlbl";
+            this.SDlbl.Size = new System.Drawing.Size(213, 29);
+            this.SDlbl.TabIndex = 1;
+            this.SDlbl.Text = "Standard deviation";
+            // 
+            // lblMeanResult
+            // 
+            this.lblMeanResult.AutoSize = true;
+            this.lblMeanResult.Location = new System.Drawing.Point(271, 47);
+            this.lblMeanResult.Name = "lblMeanResult";
+            this.lblMeanResult.Size = new System.Drawing.Size(0, 29);
+            this.lblMeanResult.TabIndex = 4;
+            // 
+            // lblStdDeviation
+            // 
+            this.lblStdDeviation.AutoSize = true;
+            this.lblStdDeviation.Location = new System.Drawing.Point(296, 127);
+            this.lblStdDeviation.Name = "lblStdDeviation";
+            this.lblStdDeviation.Size = new System.Drawing.Size(0, 29);
+            this.lblStdDeviation.TabIndex = 5;
+            // 
+            // lblHistogram
+            // 
+            this.lblHistogram.AutoSize = true;
+            this.lblHistogram.Location = new System.Drawing.Point(50, 226);
+            this.lblHistogram.Name = "lblHistogram";
+            this.lblHistogram.Size = new System.Drawing.Size(123, 29);
+            this.lblHistogram.TabIndex = 6;
+            this.lblHistogram.Text = "Histogram";
+            // 
+            // lblHistogramResults
+            // 
+            this.lblHistogramResults.AutoSize = true;
+            this.lblHistogramResults.Location = new System.Drawing.Point(61, 283);
+            this.lblHistogramResults.Name = "lblHistogramResults";
+            this.lblHistogramResults.Size = new System.Drawing.Size(0, 29);
+            this.lblHistogramResults.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1039, 764);
+            this.Controls.Add(this.lblHistogramResults);
+            this.Controls.Add(this.lblHistogram);
+            this.Controls.Add(this.lblStdDeviation);
+            this.Controls.Add(this.lblMeanResult);
+            this.Controls.Add(this.SDlbl);
+            this.Controls.Add(this.lblMean);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblMean;
+        private System.Windows.Forms.Label SDlbl;
+        private System.Windows.Forms.Label lblMeanResult;
+        private System.Windows.Forms.Label lblStdDeviation;
+        private System.Windows.Forms.Label lblHistogram;
+        private System.Windows.Forms.Label lblHistogramResults;
     }
 }
 
--- SemanticTest/StatisticsApp/Form1.cs
+++ SemanticTest/StatisticsApp/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,114 @@ namespace StatisticsApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int countDataSetValues = 0;
+            var sampleDataValues = ReadFile();
+            double mean = CalculateMean(ref countDataSetValues, sampleDataValues);
+            int[] histogram = new int[10];
+            int upperLimit = 0;
+            lblMeanResult.Text =mean.ToString(); 
+            lblStdDeviation.Text = CalculateStddeviation(countDataSetValues,mean, sampleDataValues).ToString();           
+            histogram = CalculateHistogram(sampleDataValues);
+            lblHistogramResults.Text = "";
+            for (int i = 0; i < histogram.Length; i++)
+            {
+                upperLimit = i + 1;
+                lblHistogramResults.Text += "From " + i + "0 to " + upperLimit + "0 there are " + histogram[i] + " numbers \n";
+            }
+        }
+
+        private List<double> ReadFile()
+        {
+            List<double> values = new List<double>();
+            try
+            {                
+                using (var reader = new StreamReader(Application.StartupPath +
+                   "\\Data\\SampleData.csv"))
+                {
+                    List<double> sampleDataNumbers = new List<double>();
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        values = line.Split(',').Select(double.Parse).ToList();
+                    }
+                }                
+            }
+            catch (PathTooLongException pathTooLong)
+            {
+                MessageBox.Show("PathTooLongException " + pathTooLong.Message);
+            }
+            catch (DirectoryNotFoundException directoryNotFound)
+            {
+                MessageBox.Show("DirectoryNotFoundException " + directoryNotFound.Message);
+            }
+            catch (UnauthorizedAccessException unauthorizedAccess)
+            {
+                MessageBox.Show("UnauthorizedAccessException " + unauthorizedAccess.Message);
+            }
+            catch (FileNotFoundException fileNotFound)
+            {
+                MessageBox.Show("FileNotFoundException " + fileNotFound.Message);
+            }
+            catch (NotSupportedException notSupported)
+            {
+                MessageBox.Show("NotSupportedException " + notSupported.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Generic" + ex.Message);
+            }
+                return values;            
+        }
+
+        private double CalculateMean(ref int countOfValues, List<double> sampleDataValues)
+        {
+            double mean = 0;
+            double sum = 0;
+            // I can get the size of the list by doing sampleDataValues.Count but accordingly to the
+            // instructions I should use built in functionality as little as possible
+            countOfValues = 0;            
+            foreach (var dataValue in sampleDataValues)
+            {
+                sum += dataValue;
+                countOfValues++;
+            }
+            mean = (sum / countOfValues);
+            return mean;
+        }
+
+        private double CalculateStddeviation(int countDataSetValues, double mean, List<double> sampleDataValues)
+        {
+            double stdDeviation = 0;
+            double sumDistances = 0;
+            double squareOfDistanceFromMean = 0;
+            foreach (var dataValue in sampleDataValues)
+            {
+                // I can use Math.Pow for square but accordingly to the
+                // instructions I should use built in functionality as little as possible
+                squareOfDistanceFromMean = (dataValue - mean) * (dataValue - mean);
+                sumDistances += squareOfDistanceFromMean;
+            }
+            stdDeviation= Math.Sqrt(sumDistances/ countDataSetValues);
+            return stdDeviation;
+        }
+
+        //to avoid a huge switch/else if statement I ll use mod because there is
+        //a relation between the index of the table and the bins of 10.
+        //Code is cleaner and more efficient.
+        private int[] CalculateHistogram(List<double> sampleDataValues)
+        {
+            int[] histogram = new int[10];
+            int index;
+            foreach (var dataValue in sampleDataValues)
+            {
+                index = (int)(Math.Floor(dataValue / 10));
+                histogram[index]++;
+            }
+            return histogram;
         }
     }
 }
